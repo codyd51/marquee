a44fee550237fd2b7b3c7de9002df2e5b38a6cf6
Phillip Tennen <phillip.ennen@gmail.com>
2017-09-22 12:55:07 -0400

    More splitting of lib from client

--- makefile
+++ makefile
@@ -1,3 +1,13 @@
 all:
-	gcc marquee.c -o marquee
+	gcc -c marquee.c -o marquee.o
+	ar rc libmarquee.a marquee.o
+	ranlib libmarquee.a
+	gcc -c client.c -o client.o
+	gcc -g -o client client.o libmarquee.a
 
+objc:
+	clang -fobjc-arc -lc++ -framework Foundation -framework AppKit draw.mm -o draw
+
+clean:
+	rm *.o
+	rm libmarquee.a
--- marquee.c
+++ marquee.c
@@ -4,31 +4,31 @@
 #include <stdbool.h>
 #include <string.h>
 #include <memory.h>
+#include "marquee.h"
 #include "font8x8_basic.h"
+
+#define UNSCALED_FONT_WIDTH  8
+#define UNSCALED_FONT_HEIGHT 8
 
 void mq_px_on(int x, int y);
 void mq_px_off(int x, int y);
 
-#define VIEWPORT_WIDTH 100
-#define VIEWPORT_HEIGHT 9
-bool viewport[VIEWPORT_HEIGHT][VIEWPORT_WIDTH] = {0};
+static int mq_width = 0;
+static int mq_height = 0;
 
-int mq_width(void) {
-	return VIEWPORT_WIDTH;
-}
-
-int mq_height(void) {
-	return VIEWPORT_HEIGHT;
+void mq_init(int width, int height) {
+		mq_width = width;
+		mq_height = height;
 }
 
 bool verify_coord(int x, int y) {
-	if (x < 0 ||
-	    x >= mq_width() ||
-	    y < 0 ||
-	    y >= mq_height()) {
-		return false;
-	}
-	return true;
+		if (x < 0 ||
+			x >= mq_width ||
+			y < 0 ||
+			y >= mq_height) {
+				return false;
+		}
+		return true;
 }
 
 void render_char(char ch, int x, int y) {
@@ -47,50 +47,11 @@ void render_char(char ch, int x, int y) {
 }
 
 void render_string(char* str, uint32_t len, int x, int y) {
-	char* p = str;
-#define FONT_WIDTH 8
-#define FONT_HEIGHT 8
-	while (*p && (len--) != 0) {
-			render_char(*p, x, y);
-			x += FONT_WIDTH;
-			p++;
-	}
-}
-
-void mq_px_on(int x, int y) {
-	if (!verify_coord(x, y)) return;
-	viewport[y][x] = 1;
-}
-
-void mq_px_off(int x, int y) {
-	if (!verify_coord(x, y)) return;
-	viewport[y][x] = 0;
-}
-
-void print_viewport(void) {
-	for (int y = 0; y < VIEWPORT_HEIGHT; y++) {
-		for (int x = 0; x < VIEWPORT_WIDTH; x++) {
-			printf("%d", (int)viewport[y][x]);
+		char* p = str;
+		while (*p && (len--) != 0) {
+				render_char(*p, x, y);
+				x += UNSCALED_FONT_WIDTH;
+				p++;
 		}
-		printf("\n");
-	}
 }
 
-int main(int argc, char** argv) {
-	memset(viewport, 0, sizeof(viewport));
-
-	/*
-	for (int i = '!'; i < '~'; i++) {
-			printf("\n%c: \n", i);
-			render_char(i, 0, 0);
-			print_viewport();
-			usleep(2000 * 1000);
-	}
-	*/
-
-	char* msg = "Hello World!";
-	render_string(msg, strlen(msg), 1, 1);
-	print_viewport();
-
-	return 0;
-}
